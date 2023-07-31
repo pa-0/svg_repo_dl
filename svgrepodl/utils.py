57ab14251034ac7d7d7317ab1928d0012e5fb704
Andreas Rozek <A.Rozek@gmx.de>
2023-07-31 11:37:57 +0200

    Update utils.py

--- svgrepodl/utils.py
+++ svgrepodl/utils.py
@@ -20,12 +20,13 @@ def runBrowser(driver, url):
 	driver.get(url)
 	time.sleep(3) #REACT app need to sleep and wait app load.
 	print('got',url)
-	all_links=driver.execute_script('all_links = []; links = document.querySelectorAll(".style-module--action--1Avvt>a"); links.forEach(url => all_links.push(url.href)); return all_links');
-	print('found',all_links)
-	bar = IncrementalBar('📥 Icons Downloaded', max = len(all_links))
 	
+	all_links=driver.execute_script('all_links = []; links = document.querySelectorAll(".style-module--action--1Avvt>a"); links.forEach(url => all_links.push(url.href)); return all_links');
+	print('found the following links which have to be downloaded:\n',all_links)
+	
+	bar = IncrementalBar('📥 Icons Downloaded', max = len(all_links))
 	for i, link in  enumerate(all_links):
-		print(link,'\n')
+		print('downloading',link,'\n')
 		driver.execute_script('''window.open("'''+link+'''","_blank");''')
 		bar.next()
 	print('\n')
